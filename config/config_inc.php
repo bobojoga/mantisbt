e822777aefae222bf367f8c3674334813df29369
Josip Rakić <josip.rakic@igea.hr>
2018-10-04 10:56:22 +0200

    Removed CAS auth for time being

--- config/config_inc.php
+++ config/config_inc.php
@@ -42,7 +42,8 @@ $g_cas_ldap_update = '';
 $g_ldap_language_keys = '';
 $g_ldap_language_values = '';
 $g_path = '../cas/';
-$g_login_method = 'CAS_AUTH';
+#$g_login_method = 'CAS_AUTH';
+
 $g_log_level = 'ALL';
 $g_log_destination = 'file:/var/www/html/mantis/temp/mantis.log';
 
