e9d6a2d57b94509493065b4840082a4a24c8956d
Josip Rakić <josip.rakic@igea.hr>
2018-10-16 14:15:59 +0200

    Added print button for bug list and css fix for printing

--- js/custom_sdge.js
+++ js/custom_sdge.js
@@ -9,4 +9,32 @@ $(function() {
     $this.closest("tr").css("background-color", bgCol);
   });
   
+  
+  $(".fa-file-word-o").each(function() {
+      var $this = $(this);
+      var childNode = $this[0];
+
+      if (childNode.title == "Word 2000") {
+          var printLink = document.createElement("a");
+          printLink.href = "#";
+
+          printLink.addEventListener("click", function(e) {
+                  createPrintDialog();
+          }, false);
+
+          iconElement = document.createElement("i");
+          iconElement.classList.add("fa");
+          iconElement.classList.add("fa-print");
+          iconElement.title = "Ispis stranice";
+
+          printLink.appendChild(iconElement);
+          printLink.innerHTML += '&nbsp;';
+
+          childNode.parentNode.before(printLink);
+      }
+    });
 });
+
+function createPrintDialog(){
+  window.print();
+}
