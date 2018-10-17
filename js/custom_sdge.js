f6e2e2c130ff547886bfcc2101f48ff11dbd0c5b
Josip Rakić <josip.rakic@igea.hr>
2018-10-17 13:32:49 +0200

    Dodana mogucnost odustajanja od nove prijave, povratak botun

--- js/custom_sdge.js
+++ js/custom_sdge.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	//bojanje redova kod prikaza bugova
 	$(".fa-status-box").each(
 			function() {
 				var $this = $(this);
@@ -11,6 +12,7 @@ $(function() {
 				$this.closest("tr").css("background-color", bgCol);
 			});
 
+	//botun za printanje kod liste kada se hoce printat
 	$(".fa-file-word-o").each(function() {
 		var $this = $(this);
 		var childNode = $this[0];
@@ -35,6 +37,7 @@ $(function() {
 		}
 	});
 
+	//back botun da se vrati sa liste di se printa
 	$(".bigger-120").each(function() {
 		var $this = $(this);
 		var childNode = $this[0];
@@ -58,8 +61,47 @@ $(function() {
 		backDiv.appendChild(backA);
 
 		childNode.before(backDiv);
-
 	});
+	
+// Kada se rijesi domena mantisa, same origin policy blokira ovo ako je drugacija domena od parenta
+//    $('label[for="custom_field_29"]').each(function() {
+//    	
+//    	var nesto = parent.document.getElementById("imeIPrezimeKorisnika");
+//    
+//	});
+    
+    var currentUrlIframe = window.location.href;
+
+    //true ako je trenutni page bug_report_page.php
+    if (currentUrlIframe.indexOf('bug_report_page.php') !== -1) {
+
+            $("#breadcrumbs").each(function() {
+                    var $this = $(this);
+                    var childNode = $this[0];
+
+                    var backDiv = document.createElement("div");
+                    backDiv.classList.add("btn-group");
+
+                    var backA = document.createElement("a");
+                    backA.href = "#";
+                    backDiv.classList.add("btn");
+                    backDiv.classList.add("btn-primary");
+                    backDiv.classList.add("btn-xs");
+                    backDiv.classList.add("btn-white");
+                    backDiv.classList.add("btn-round");
+
+                    backA.addEventListener("click", function(e) {
+                            goBack();
+                    }, false);
+
+                    backA.innerHTML += "Povratak";
+                    backDiv.appendChild(backA);
+
+                    childNode.appendChild(backDiv);
+                    
+            });
+
+    }
 
 });
 
